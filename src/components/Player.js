e6047ea5cd95a55ff11e721dfe94b2f79a5435e4
Harry Stead <harrystead9@gmail.com>
2021-02-01 13:02:42 +0000

    input slider and time values(duration and currentTime added)

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,34 +1,71 @@
-import React, {useRef} from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPlay, faAngleLeft, faAngleRight} from "@fortawesome/free-solid-svg-icons";
+import React, { useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
-    const audioRef = useRef(null);
-    const audioPlay = () => {
-        if(isPlaying){
-            audioRef.current.pause();
-            setIsPlaying(!isPlaying);
-        }
-        else{
-            audioRef.current.play();
-            setIsPlaying(!isPlaying);
-        }
+  const audioRef = useRef(null);
+  const audioPlay = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
     }
-    return(
-        <div className="player">
-            <div className="time-control">
-                <p>Start Time</p>
-                <input type="range"/>
-                <p>End Time</p>
-            </div>
-            <div className="play-control">
-                <FontAwesomeIcon className="skipBack" size="2x" icon={faAngleLeft}/>
-                <FontAwesomeIcon onClick={audioPlay} className="play" size="2x" icon={faPlay}/>
-                <FontAwesomeIcon className="skipForward" size="2x" icon={faAngleRight}/>
-            </div>
-            <audio ref={audioRef} src={currentSong.audio}></audio>
-        </div>
-    )
-}
+  };
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    console.log(current);
+
+    setSongInfo({...songInfo, currentTime: current, duration: duration });
+  };
+
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
+
+  const getTime = (time) => {
+      return(
+          Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+      );
+  };
+
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>{getTime(songInfo.currentTime)}</p>
+        <input min={0} max={songInfo.duration} value={songInfo.currentTime} type="range" />
+        <p>{getTime(songInfo.duration)}</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon className="skipBack" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon
+          onClick={audioPlay}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
+        <FontAwesomeIcon
+          className="skipForward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
+      <audio
+        ref={audioRef}
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        src={currentSong.audio}
+      ></audio>
+    </div>
+  );
+};
 
 export default Player;
