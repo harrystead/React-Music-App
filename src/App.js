9445c2b49bab21c1e97104230f09d9a50dfc2bf8
Harry Stead <harrystead9@gmail.com>
2021-02-01 12:03:47 +0000

    pause and play functionality working

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import data from './data';
 function App () {
 const [songs, setSongs] = useState(data());
 const [currentSong, setCurrentSong] = useState(songs[0]);
+const [isPlaying, setIsPlaying] = useState(false);
 return(
   <div className="app"> 
    <Songs currentSong={currentSong} />
-   <Player currentSong={currentSong}/>
+   <Player currentSong={currentSong} isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
   </div>
 )
 }
