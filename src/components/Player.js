4d55b987558400d5e1367e4ebd4cb9cb2c62d99f
Harry Stead <harrystead9@gmail.com>
2021-02-01 13:13:06 +0000

    fixed bugs

--- src/components/Player.js
+++ src/components/Player.js
@@ -21,27 +21,37 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    console.log(current);
 
-    setSongInfo({...songInfo, currentTime: current, duration: duration });
+    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
+  };
+
+  const dragHandler = (e) => {
+      audioRef.current.currentTime = e.target.value;
+      setSongInfo({...songInfo, currentTime: e.target.value});
   };
 
   const [songInfo, setSongInfo] = useState({
-    currentTime: null,
-    duration: null,
+    currentTime: "",
+    duration: "",
   });
 
   const getTime = (time) => {
-      return(
-          Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
-      );
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
   };
 
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input min={0} max={songInfo.duration} value={songInfo.currentTime} type="range" />
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+          type="range"
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
